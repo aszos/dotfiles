52dc65c9b371b0a18cc2953ab5c899c4444de434
Alex Szostek <8537352+aszos@users.noreply.github.com>
2022-01-20 10:51:35 -0500

    update README, restructure

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # dotfiles
+
+## About
+
+### What's inside?
+
+    .
+    ├── install.sh						# Moves / Symlinks Dotfiles & Folders into Filesystem                   
+    ├── bin                   
+		│   ├── .alias            # Rename Commands / Set Default Parameters
+		│   ├── .funcs            # Custom Functions
+		│   ├── .path             # Configure $PATH
+		│   ├── .projects         # Project File Structure
+		│   ├── .prompt           # Command Line Prompt Configuration
+		│   ├── .ssh-setup        # SSH Agent & SSH Keys Setup
+		│   ├── .tmux.conf        # tmux Configuration
+		│   ├── .urls             # URLs
+		│   ├── .vars             # Misc. Environment Variables
+		│   ├── .zprofile         # zsh Profile Configuration
+		│   ├── .zshrc            # zsh Session Configuration
+    ├── git                   
+		│   ├── .gitconfig        # Git Configuration
+		│   ├── .githooks
+		│   │   ├── hooks         # Functions to run during Git commit/push/etc.
+    └── README.md							# The file you're reading right now!
+
+
+**Installing**
+```console
+$ chmod +x install.sh
+$ ./install.sh
+```
--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
 #!/bin/zsh
 echo ">> Setting environment variables..."
-export DOTFILES_PROJECT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-DOT_VARS=$DOTFILES_PROJECT/src/bin/.vars
-source $DOT_VARS
+source bin/.projects
+source bin/.vars
 
 echo ">> Installing dotfiles..."
 #dotfiles/$dir --> ${HOME}/.*
 dotfiles_dirs=(git)
 for dir in ${dotfiles_dirs[@]}; do
-	for file in ${DOTFILES_SRC}/${dir}/.[a-z]*; do
+	for file in ${DOTFILES_PROJECT}/${dir}/.[a-z]*; do
 		$EXEC cp -r $file ${HOME}/"$(basename ${file})"
 	done
 done
@@ -16,21 +15,21 @@ done
 #dotfiles/bin/.* ~~> ${HOME}/.*
 dotfiles_bin=(bin)
 for dir in ${dotfiles_bin[@]}; do
-	for file in ${DOTFILES_SRC}/${dir}/.[a-z]*; do
-		$EXEC ln -s $file ${HOME}/"$(basename ${file})"
+	for file in ${DOTFILES_PROJECT}/${dir}/.[a-z]*; do
+		$EXEC ln -vfns $file ${HOME}/"$(basename ${file})"
 	done
 done
 
 #$dir --> ${HOME}/.$dir
 dot_dirs=(vim aws ssh)
 for dir in ${dot_dirs[@]}; do
-	$EXEC ln -s ${DOTFILES_BIN}/$dir ${HOME}/.$dir
+	$EXEC ln -vfns ${DOTFILES_BIN}/$dir ${HOME}/.$dir
 done
 
 #$dir --> ${HOME}/$dir
 dirs=(python)
 for dir in ${dirs[@]}; do
-	$EXEC ln -s ${DOTFILES_BIN}/$dir ${HOME}/$dir
+	$EXEC ln -vfns ${DOTFILES_BIN}/$dir ${HOME}/$dir
 done
 
 echo ">> Done!"
